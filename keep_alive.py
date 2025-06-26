f3cd215e8eba3684ba9238eddc868456bea6f67e
redxsmoke <andrewevans9560@gmail.com>
2025-06-26 14:50:19 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,9 +9,9 @@ def home():
     return "I am alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8080))  # Use env PORT or default 8080
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-
