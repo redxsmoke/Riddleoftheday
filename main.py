856f80a0d325f4af567c3f5ac0f33931f9e56f9a
redxsmoke <andrewevans9560@gmail.com>
2025-07-11 00:42:04 -0400

    asd

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from views import LeaderboardView, create_leaderboard_embed
 from db import create_db_pool, upsert_user, get_user, insert_submitted_question, get_all_submitted_questions, increment_score, increment_streak, get_score, get_all_scores_and_streaks
 
 
-from seedriddles import seed_riddles
+
 
 
 intents = discord.Intents.default()
@@ -552,7 +552,7 @@ async def run_bot():
         print(f"❌ Failed to connect to the database: {e}")
         exit(1)
 
-    await seed_riddles()
+    
     await client.start(TOKEN)
 
 
